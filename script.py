930bb666848930f9a43cd45dcabc283e79a663d0
Brian Lobo <brian.lobo@moorea.io>
2020-03-10 22:31:39 -0300

    nueva version

--- script.py
+++ script.py
@@ -1,21 +1,2 @@
-from ambiente import parsear_variables_de_ambiente
-import argparse
-
-import yaml
-
-_NOMBRE_ARCHIVO_YML = 'config.yml'
-_AYUDA_ARCHIVO_YML = 'Archivo de parametros .yml'
-_NOMBRE_CORTO_YML = '-f'
-_NOMBRE_LARGO_YML = '--file'
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument(_NOMBRE_CORTO_YML, _NOMBRE_LARGO_YML,
-                    help=_AYUDA_ARCHIVO_YML, default=_NOMBRE_ARCHIVO_YML)
-
-args = parser.parse_args()
-
-params_yaml = yaml.safe_load(open(args.file))
-
-
-print(parsear_variables_de_ambiente(params_yaml))
+def ejecutar(config: dict):
+    print(config)
