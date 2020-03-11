fba6bdecb4a29dd3439b1ed93e976e8d4e316b06
Brian Lobo <brian.lobo@moorea.io>
2020-03-10 22:33:34 -0300

    mejoras

--- iniciar.py
+++ iniciar.py
@@ -1,8 +1,9 @@
-from ambiente import parsear_variables_de_ambiente
-from script import ejecutar
 import argparse
 
 import yaml
+
+from ambiente import parsear_variables_de_ambiente
+from script import ejecutar
 
 _NOMBRE_ARCHIVO_YML = 'config.yml'
 _AYUDA_ARCHIVO_YML = 'Archivo de parametros .yml'
--- script.py
+++ script.py
@@ -1,2 +1,5 @@
 def ejecutar(config: dict):
+    '''
+    Solo se deberia tocar esta parte del codigo, lo otro NO
+    '''
     print(config)
