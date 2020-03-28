40767f34bcb737902c09bd8957b8c0f1964da7ba
Brian Lobo <brian.d.lobo@gmail.com>
2020-03-28 15:14:42 -0300

    refactor de codigo

--- scripts/pyinstaller/build.sh
+++ scripts/pyinstaller/build.sh
@@ -1,26 +1,23 @@
-. ../scripts/pyinstaller/ambiente.env
+. ./scripts/pyinstaller/ambiente.env
 
 # cd ${CARPETA_PROYECTO}
 
 ls
 
-printf "### Borrando archivos generados anteriores \n\n"
+printf "\nBorrando archivos generados anteriores \n"
 rm -fr dist/
 
 
-printf "### Ejecutando Pyinstaller \n\n"
+printf "\nEjecutando Pyinstaller \n\n"
 pyinstaller \
-${ARCHIVO_PY_INICIADOR} \
+${CARPETA_PROYECTO}${ARCHIVO_PY_INICIADOR} \
 --clean \
 --onefile \
 --name ${NOMBRE_PROYECTO} \
 --log-level ${NIVEL_LOG} \
---add-data="version.txt:resources" \
-# --specpath specs \
-# --distpath dist \
-# --workpath build
+--add-data="src/resources/version.txt:resources" \
 
-printf "### Borrando archivos innecesarios \n\n"
-rm -fr build/ specs/
+printf "\nBorrando archivos innecesarios \n\n"
+rm -fr build/ *.spec
 
 # cd ..
--- src/iniciar.py
+++ src/iniciar.py
@@ -1,41 +1,49 @@
 import argparse
-import os
 import sys
+from os import path
 
 import yaml
 
 from ambiente import obtener_valor, parsear_variables_de_ambiente
 from script import ejecutar
 
+_ruta_base = sys._MEIPASS if hasattr(sys, '_MEIPASS') else ''
+_ruta_resources = path.join(_ruta_base, 'resources')
+_ruta_archivo_version = path.join(_ruta_resources, 'version.txt')
+_ruta_archivo_config_yml = path.join(_ruta_base, 'config.yml')
 
-def cargar_ruta_resources() -> str:
-    return f'{sys._MEIPASS}/resources' if hasattr(
-        sys, '_MEIPASS') else 'resources'
 
-
-def cargar_version_script():
-    with open(f'{_ruta_resources}/version.txt') as version_archivo:
+def cargar_version_script(ruta_archivo: str):
+    with open(ruta_archivo) as version_archivo:
         return version_archivo.read()
 
 
 def cargar_config_yml(ruta_config_yml: str) -> dict:
+    if not path.exists(ruta_config_yml):
+        salir_con_error(
+            f'Archivo yml de configuracion {ruta_config_yml} no encontrado')
+
     with open(ruta_config_yml) as params_yaml_archivo:
-        params_yaml = yaml.safe_load(params_yaml_archivo)
+        try:
+            params_yaml = yaml.safe_load(params_yaml_archivo)
+        except Exception as _:
+            salir_con_error('Archivo yml no valido')
 
     return parsear_variables_de_ambiente(params_yaml)
 
 
+def salir_con_error(mensaje: str):
+    print(f'Error: {mensaje}')
+    sys.exit(0)
+
+
 if __name__ == "__main__":
-
-    _ruta_resources = cargar_ruta_resources()
-
-    _version_script = cargar_version_script()
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-f', '--file', help='Archivo de parametros .yml', default='config.yml')
+        '-f', '--file', help='Archivo de parametros .yml', default=_ruta_archivo_config_yml)
     parser.add_argument('-v', '--version',  help='Version del script',
-                        action='version', version=_version_script)
+                        action='version', version=cargar_version_script(_ruta_archivo_version))
 
     args = parser.parse_args()
 
